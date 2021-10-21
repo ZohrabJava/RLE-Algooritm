ea1d0026b3eceddb110f26bf509d96c030ad093c
ZohrabJS <zoro.77zz777@mail.ru>
2021-10-21 19:42:43 +0400

    Add project files.

---
+++ RLE/RLE.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	char str[255];
+	char b;
+	int i, n;
+	cout << "Enter the line: ";
+	cin.get(str, 255);
+	n = 1;
+	b = str[0];
+	cout << "Enter RLE coded test: ";
+	for (i = 1; i <= strlen(str) - 1; i++) 
+		if (str[i] == b) 
+			n++;
+	
+		else
+		{
+			if (n != 1)
+				cout << b << n;
+
+			else
+			    cout << b << n;	
+			    n = 1;
+			    b = str[i];
+		}
+	if (n == 1) {
+		cout << b << n;
+	}
+	else
+		cout << b << n;
+	
+	
+	return 0;
+}
+
